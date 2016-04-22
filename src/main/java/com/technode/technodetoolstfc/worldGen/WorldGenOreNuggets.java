605cab0305aa14f69129b0f81d3dfdee873da7da
Bunsan <bunsan@users.noreply.github.com>
2016-04-21 19:46:37 -0500

    Nugget Debris Generation

---
+++ src/main/java/com/technode/technodetoolstfc/worldGen/WorldGenOreNuggets.java
@@ -0,0 +1,80 @@
+package com.technode.technodetoolstfc.worldGen;
+
+import com.bioxx.tfc.TileEntities.TEWorldItem;
+import com.bioxx.tfc.WorldGen.TFCBiome;
+import com.bioxx.tfc.api.TFCBlocks;
+import com.technode.technodetoolstfc.block.BlockMetalOre;
+import com.technode.technodetoolstfc.core.ModBlocks;
+import com.technode.technodetoolstfc.core.utility.LogHelper;
+import cpw.mods.fml.common.IWorldGenerator;
+import net.minecraft.block.material.Material;
+import net.minecraft.init.Blocks;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+import net.minecraft.world.chunk.IChunkProvider;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class WorldGenOreNuggets implements IWorldGenerator {
+    public WorldGenOreNuggets() {
+    }
+
+    public boolean generateRocks(World world, Random random, int i, int j, int k) {
+        if ((world.isAirBlock(i, j + 1, k) || world.getBlock(i, j + 1, k) == Blocks.snow || world.getBlock(i, j + 1, k) == TFCBlocks.tallGrass) &&
+                (world.getBlock(i, j, k).getMaterial() == Material.grass || world.getBlock(i, j, k).getMaterial() == Material.rock) && world.getBlock(i, j, k).isOpaqueCube()) {
+            ItemStack sample = getCoreSample(world, i, j, k);
+            if (world.rand.nextInt(3) == 0 && sample != null) {
+                if (world.setBlock(i, j + 1, k, TFCBlocks.worldItem, 0, 2)) {
+                    TEWorldItem te = (TEWorldItem) world.getTileEntity(i, j + 1, k);
+                    te.storage[0] = sample;
+                }
+            }
+        }
+        return true;
+    }
+
+    private ItemStack getCoreSample(World world, int xCoord, int yCoord, int zCoord) {
+        ArrayList<Item> coreSample = new ArrayList<Item>();
+        ArrayList<ItemStack> coreSampleStacks = new ArrayList<ItemStack>();
+        for (int x = -15; x < 16; x++) {
+            for (int z = -15; z < 16; z++) {
+                for (int y = yCoord; y > yCoord - 35; y--) {
+                    if (world.blockExists(xCoord + x, y, zCoord + z) && world.getBlock(xCoord + x, y, zCoord + z) == ModBlocks.oreMod) {
+                        int m = world.getBlockMetadata(xCoord + x, y, zCoord + z);
+                        if (!coreSample.contains(BlockMetalOre.getDroppedItemMod(m))) {
+                            coreSample.add(BlockMetalOre.getDroppedItemMod(m));
+                            coreSampleStacks.add(new ItemStack(BlockMetalOre.getDroppedItemMod(m), 1, m));
+                        }
+                    }
+                }
+            }
+        }
+        if (!coreSampleStacks.isEmpty())
+            return coreSampleStacks.get(world.rand.nextInt(coreSampleStacks.size()));
+        return null;
+    }
+
+    @Override
+    public void generate(Random random, int chunkX, int chunkZ, World world,
+                         IChunkProvider chunkGenerator, IChunkProvider chunkProvider) {
+        chunkX *= 16;
+        chunkZ *= 16;
+
+        if (world.getBiomeGenForCoords(chunkX, chunkZ) instanceof TFCBiome) // Fixes ClassCastException
+        {
+            TFCBiome biome = (TFCBiome) world.getBiomeGenForCoords(chunkX, chunkZ);
+            if (biome == TFCBiome.OCEAN || biome == TFCBiome.DEEP_OCEAN)
+                return;
+        }
+
+        //ModOres
+        for (int itemCount = 0; itemCount < 8; itemCount++) {
+            int xCoord = chunkX + random.nextInt(16) + 8;
+            int zCoord = chunkZ + random.nextInt(16) + 8;
+            generateRocks(world, random, xCoord, world.getTopSolidOrLiquidBlock(xCoord, zCoord) - 1, zCoord);
+        }
+
+    }
+}
